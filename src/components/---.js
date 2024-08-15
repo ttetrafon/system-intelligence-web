5e0abdfd4c8f5f91f770fd6be152c22d78d2dca3
ttetrafon <ttetrafon@yahoo.gr>
2024-08-15 16:39:04 +0300

    basic structure

---
+++ src/components/---.js
@@ -0,0 +1,33 @@
+const template = document.createElement('template');
+
+template.innerHTML = /*html*/`
+  <style>
+  </style>
+
+`;
+
+class Component extends HTMLElement {
+  constructor() {
+    super();
+    this._shadowRoot = this.attachShadow({ mode: 'closed' });
+    this._shadowRoot.appendChild(template.content.cloneNode(true));
+
+
+  }
+
+  static get observedAttributes() { return ['label']; }
+
+  get label() { return this.getAttribute('label'); }
+
+  set label(value) { this.setAttribute('label', value); }
+
+  attributeChangedCallback(name, oldVal, newVal) {
+    if (oldVal == newVal) return;
+    switch(name) {
+      default:
+        break;
+    }
+  }
+}
+
+window.customElements.define('my-component', Component);
---
+++ src/script.js
@@ -0,0 +1,5 @@
+import './components/si-footer.js';
+import './components/si-header.js';
+
+const title = document.querySelector("title");
+console.log(title.text + " script started...");
