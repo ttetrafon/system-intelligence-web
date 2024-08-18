776b8f2b428f54f982b36accb6329bb38ca5c78c
ttetrafon <ttetrafon@yahoo.gr>
2024-08-18 21:29:36 +0300

    printable character sheet++

--- src/components/cs-skill-level-entry-line.js
+++ src/components/cs-skill-level-entry-line.js
@@ -8,7 +8,10 @@ template.innerHTML = /*html*/`
 <div class="flex-row">
   <input type="checkbox" class="hidden" />
   <h6></h6>
-  <span class="entry-line"></span>
+  <span id="left-bracket" class="hidden">(</span>
+  <span id="note" class="hidden"></span>
+  <span id="right-bracket" class="hidden">)</span>
+  <span id="value" class="entry-line"></span>
   <span>[</span>
   <span class="entry-line-small"></span>
   <span>/</span>
@@ -25,16 +28,22 @@ class Component extends HTMLElement {
 
     this.$label = this._shadowRoot.querySelector("h6");
     this.$checkbox = this._shadowRoot.querySelector("input");
+    this.$span = this._shadowRoot.getElementById("value");
+    this.$leftBracket = this._shadowRoot.getElementById("left-bracket");
+    this.$rightBracket = this._shadowRoot.getElementById("right-bracket");
+    this.$note = this._shadowRoot.getElementById("note");
   }
 
-  static get observedAttributes() { return ['label', 'checkbox', 'skill']; }
+  static get observedAttributes() { return ['label', 'checkbox', 'note', 'skill']; }
 
   get label() { return this.getAttribute('label'); }
   get checkbox() { return this.getAttribute('checkbox'); }
+  get note() { return this.getAttribute('note'); }
   get skill() { return this.getAttribute('skill'); }
 
   set label(value) { this.setAttribute('label', value); }
   set checkbox(value) { this.setAttribute('checkbox', value); }
+  set note(value) { this.setAttribute('note', value); }
   set skill(value) { this.setAttribute('skill', value); }
 
   attributeChangedCallback(name, oldVal, newVal) {
@@ -42,7 +51,14 @@ class Component extends HTMLElement {
     if (oldVal == newVal) return;
     switch(name) {
       case "label":
-        this.$label.innerText = this.label;
+        if (this.label == "_") {
+          this.$label.classList.add("entry-line");
+          this.$span.classList.remove("entry-line");
+          this.$span.classList.add("entry-line-medium");
+        }
+        else {
+          this.$label.innerText = this.label;
+        }
         break;
       case "checkbox":
         switch(this.checkbox) {
@@ -68,6 +84,19 @@ class Component extends HTMLElement {
           default:
             this.$label.classList.remove("complex");
             break;
+        }
+        break;
+      case "note":
+        this.$span.classList.remove("entry-line");
+        this.$span.classList.add("entry-line-small");
+        this.$leftBracket.classList.remove("hidden");
+        this.$note.classList.remove("hidden");
+        this.$rightBracket.classList.remove("hidden");
+        if (this.note = "_") {
+          this.$note.classList.add("entry-line");
+        }
+        else {
+          this.$note.innerText = this.note;
         }
         break;
     }
