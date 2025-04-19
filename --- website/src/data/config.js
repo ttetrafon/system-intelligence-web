fdd2a49f5caf824e051cfa70848b98495cded4cc
ttetrafon <info@ttetrafon.com>
2025-04-19 11:35:06 +0300

    [web-app] removed deprecated files

--- --- website/src/data/config.js
+++
@@ -1,3 +0,0 @@
-export const urls = {
-  gameDataService: "http://localhost:3000"
-};
--- --- website/src/service/spinning-circle.js
+++
@@ -1,8 +0,0 @@
-const spinningCircle = document.createElement("loading-circle");
-document.body.appendChild(spinningCircle);
-spinningCircle.classList.add("hidden");
-
-document.addEventListener("toggle-spinning-circle", (event) => {
-  event.stopPropagation();
-  spinningCircle.classList.toggle("hidden", !event.detail.state);
-});
