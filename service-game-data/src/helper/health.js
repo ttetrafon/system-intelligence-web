bd008d6ac968e7b5707f0191dbcc8aec4143452f
ttetrafon <ttetrafon@yahoo.gr>
2025-11-16 14:00:32 +0200

    remove everything

--- service-game-data/src/helper/health.js
+++
@@ -1,3 +0,0 @@
-export function getMemoryUsage() {
-  return process.memoryUsage();
-}
--- website/src/data/config.js
+++
@@ -1,7 +0,0 @@
-// URLs
-export const domainRoot = "https://system-intelligence.com";
-export const gameServiceUrl = ""; // TODO: make this work from env, so that it uses localhost in dev mode but the service url when built
-
-// routes
-export const routeCommands = "/data/command";
-export const routeTableOfContents = "/data/web-app-menus";
