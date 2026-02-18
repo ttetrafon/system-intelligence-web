9c1f40d811251cb7844a20368f7382f86fbabe4d
ttetrafon <ttetrafon@yahoo.gr>
2026-02-18 21:29:00 +0200

    footer: visible version

--- app/components/general/Footer.tsx
+++ app/components/general/Footer.tsx
@@ -1,7 +1,10 @@
+import { version } from '../../../package.json';
+
 export default function Footer() {
   return (
-    <footer className="w-full bg-beta p-2">
-      footer...
+    <footer className="flex flex-row flex-nowrap items-center justify-end gap-1 w-full p-2 text-sm rounded-lg bg-beta">
+      <span className='flex-0 whitespace-nowrap' id="copyright">{`Copyright © 2025-${new Date().getFullYear()}`}</span>
+      <span className='flex-0 whitespace-nowrap' id="version">(version {version})</span>
     </footer>
   );
 }
